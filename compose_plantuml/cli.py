d04725c68e1680aede17423bdc8cb1c8c4ebc9f4
Thomas LEVEIL <thomasleveil@gmail.com>
2018-11-11 19:04:07 +0100

    refactor to ease tests

---
+++ compose_plantuml/cli.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import argparse
+
+from . import ComposePlantuml
+
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--link-graph', action='store_const', const=True,
+        help='prints a link graph', default=False, )
+    parser.add_argument(
+        '--boundaries', action='store_const', const=True,
+        help='prints the system boundaries',
+        default=False,
+    )
+    parser.add_argument(
+        '--port_boundaries', action='store_const', const=True,
+        help='prints the port system boundaries',
+        default=False,
+    )
+    parser.add_argument(
+        '--volume_boundaries', action='store_const', const=True,
+        help='prints the port system boundaries',
+        default=False,
+    )
+    parser.add_argument(
+        '--notes', action='store_const', const=True,
+        help='utilize notes for displaying additional information',
+        default=False,
+    )
+    parser.add_argument(
+        '--group', action='store_const', const=True,
+        help='group similar properties together',
+        default=False,
+    )
+    parser.add_argument('files', nargs=argparse.REMAINDER)
+    return parser.parse_args(argv)
+
+
+def execute(data, args):
+    plantuml = ComposePlantuml()
+    parsed = plantuml.parse(data)
+    output = ''
+
+    if args.link_graph:
+        output += plantuml.link_graph(parsed, notes=args.notes)
+    if args.boundaries:
+        output += plantuml.boundaries(parsed, args.group, notes=args.notes)
+    if args.port_boundaries:
+        output += plantuml.boundaries(parsed, args.group, notes=args.notes, ports=True, volumes=False)
+    if args.volume_boundaries:
+        output += plantuml.boundaries(parsed, args.group, notes=args.notes, ports=False, volumes=True)
+
+    return output
